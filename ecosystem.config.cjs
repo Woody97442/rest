6a5322b3b1c2e337da166fb0f53b31d62d63d977
woody97442 <woody97442@hotmail.fr>
2025-05-29 21:37:09 +0400

    rest op

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,12 +1,12 @@
 module.exports = {
   apps: [
     {
-      name: "rest",
-      script: "npm",
-      args: "run preview",
-      interpreter: "none", // ← pour que PM2 n’essaye pas d’utiliser Node
+      name: "rest", // Nom de l'app PM2
+      script: "npx", // On lance npx
+      args: "serve -s dist -l 3667", // Arguments de la commande npx
+      cwd: "/home/woody97442/htdocs/rest.wbpro.fr", // Dossier de travail
+      watch: false, // Pas de watch (prod)
       env: {
-        PORT: 3667,
         NODE_ENV: "production",
       },
     },
