079e4bf6da9e141a9caf0c5fd27fae8bf1bae3c4
woody97442 <woody97442@hotmail.fr>
2025-05-29 20:34:21 +0400

    action

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
       name: "rest",
       script: "npm",
       args: "run preview",
+      interpreter: "none", // ← pour que PM2 n’essaye pas d’utiliser Node
       env: {
         PORT: 3667,
         NODE_ENV: "production",
