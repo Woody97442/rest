97215149117b10fc32de7f1383d8855a54f5546a
woody97442 <woody97442@hotmail.fr>
2025-06-24 14:46:49 +0400

    up

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -16,7 +16,7 @@ export const Hero = () => {
           initial={{ opacity: 0, y: -40 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8 }}>
-          Découvrez des offres exceptionnelles
+          Découvrez des offres
         </motion.h1>
 
         <motion.p
